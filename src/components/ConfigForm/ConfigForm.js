1f1cafa8d6a287304acf627aa0460db0b486232f
mattrudin <matthias.rudin@live.com>
2018-09-23 11:47:26 +0200

    Added functionality to ConfigForm.js

--- src/components/ConfigForm/ConfigForm.js
+++ src/components/ConfigForm/ConfigForm.js
@@ -1,15 +1,44 @@
 import React from 'react';
 import './ConfigForm.css';
-import ConfigFormInput from '../ConfigFormInput/ConfigFormInput';
 import { resources } from './resources';
+import Bexio from '../../utilities/bexio';
 
 class ConfigForm extends React.Component{
 	constructor(props) {
 		super(props);
 		this.state = {
-			resources: { value: 'article'}
+			client_id: '',
+			client_secret: '',
+			resources: 'article'
 		};
+		this.handleChange = this.handleChange.bind(this);
+		this.handleIdChange = this.handleIdChange.bind(this);
+		this.handleSecretChange = this.handleSecretChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
 	}
+
+	handleSubmit(event) {
+	    Bexio.getData(this.state.client_id, this.state.client_secret);
+	    event.preventDefault();
+	}
+
+	handleIdChange(event) {
+		this.setState({
+			client_id: event.target.value
+		});
+	}
+
+	handleSecretChange(event) {
+		this.setState({
+			client_secret: event.target.value
+		});
+	}
+
+	handleChange(event) {
+    	this.setState({
+    		resources: event.target.value
+    	});
+  	}
 
 	render() {
 
@@ -17,12 +46,18 @@ class ConfigForm extends React.Component{
 
 		return(
 			<form onSubmit={this.handleSubmit}>
-       			<ConfigFormInput title="Client ID" />
-       			<ConfigFormInput title="Client Secret" />
-       			<select value={this.state.value} onChange={this.handleChange}>
+				<label>
+		          Client ID:
+		          <input type="text" value={this.state.client_id} onChange={this.handleIdChange} />
+		        </label>
+		        <label>
+		          Client Secret:
+		          <input type="text" value={this.state.client_secret} onChange={this.handleSecretChange} />
+		        </label>
+       			<select value={this.state.resources} onChange={this.handleChange}>
        				{dropdownList}
           		</select>
-        	<input type="submit" value="Submit" />
+        		<input type="submit" value="Get data from Bexio" />
       		</form>
 		);
 	}
--- src/utilities/bexio.js
+++ src/utilities/bexio.js
@@ -17,56 +17,64 @@
 //5. Call any resource
 //		see getData
 
+let accessToken = '';
 const authorizeUrl = 'https://office.bexio.com/oauth/authorize';
-const clientID;
 const redirectURI = 'http://localhost:3000/';
-const state;
+const state = '';
 
-const code;
-const accessToken;
 const accessTokenUrl = 'https://office.bexio.com/oauth/access_token';
-const clientSecret;
 
-const organisation = //company identifier
+//const organisation = //company identifier
 const resource = resource;
 
-const dataUrl = 'https://office.bexio.com/api2.php/'
+const dataUrl = 'https://office.bexio.com/api2.php/';
 
 
 
-const Bexio = () => {
-	getAccessToken() {
+const Bexio = {
+	getAccessToken(clientID, clientSecret) {
+		console.log('getAccessToken is currently running');
 		if (accessToken) {
 			return accessToken;
 		} 
-
-		//step 1 & 2
-		const authorizeEndpoint = `${authorizeUrl}?client_id=${clientID}&redirect_uri=${redirectURI}&state=${state}`;
-		window.location = authorizeEndpoint;
-		code = window.location.href.match(/code=([^&]*)/);
-		
-		//step 3 & 4
+		//step 1 & 2: get Code
+		const code = window.location.href.match(/code=([^&]*)/);
+		const expiresIn = window.location.href.match(/expires_in=([^&]*)/);
+		if(code && expiresIn) {
+			window.setTimeout(() => accessToken = '', expiresIn * 1000);
+			window.history.pushState('Code', null, '/');
+			return code;
+		} else {
+			window.location = `${authorizeUrl}?client_id=${clientID}&redirect_uri=${redirectURI}&state=${state}`
+		}
+		//step 3 & 4: get AccessToken
 		return fetch(accessTokenUrl, {
-			method: 'post';
+			method: 'post',
 			client_id: clientID,
 			redirect_uri: redirectURI,
 			client_secret: clientSecret,
 			code: code
-		})	.then(response => return response.json())
-			.then(jsonResponse => return accessToken = jsonResponse.access_token)
+		}).then(response => {return response.json()
+		}).then(jsonResponse => {return accessToken = jsonResponse.access_token
+		});
 	},
-	getData() {
-		let token = Bexio.getAccessToken();
+
+	getData(clientID, clientSecret) {
+		console.log('getData is currently running', clientID, clientSecret);
+		let token = Bexio.getAccessToken(clientID, clientSecret);
 		const dataUrl = 'https://office.bexio.com/api2.php/';
 		const organisation = '%org%';
 		const resource = 'article';
 		const url = `${dataUrl}${organisation}/${resource}`;
 		return fetch(url, {
 			headers: {
-				Accept: application/json,
+				Accept: 'application/json',
 				Authorization: `Bearer ${token}`
 		}
-		})	.then(response => return response.json())
-			.then(jsonResponse => console.log(jsonResponse))
-	};
+		}).then(response => {return response.json()
+		}).then(jsonResponse => {console.log(jsonResponse)
+		});
+	}
 }
+
+export default Bexio;
